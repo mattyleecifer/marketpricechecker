26c9d0e171ba5e5ea5a179a7981d40d6cba8cae6
mattyleecifer <36178203+mattyleecifer@users.noreply.github.com>
2022-02-19 18:01:28 +1000

    Update coinprice.py

--- coinprice.py
+++ coinprice.py
@@ -1,18 +1,18 @@
 import requests
 from datetime import datetime
 coin = input("Coin name:\n")
-date = input("Enter date in ISO8601 (up to 100 days before):\n")
+date = input("Enter date in ISO8601 separated with dashes eg 2022-01-13 (up to 100 days before):\n")
 
 global_url = "https://min-api.cryptocompare.com/data/v2/histoday?fsym=" + coin + "&tsym=AUD&limit=100"
 request = requests.get(global_url)
 results = request.json()
 data = results['Data']['Data']
-pricedata = []
+pricedata = {}
 
 for i in range(0, len(data)):
     timestamp = data[i]["time"]
     dt_object = str(datetime.fromtimestamp(timestamp))
     dt = dt_object.replace(" 10:00:00", "")
-    dt = dt.replace("-", "")
-    if dt == date:
-        print("The lowprice on " + date + " was: " + str(data[i]["low"]))
+    pricedata[dt] = data[i]["low"]
+
+print("The lowprice on " + date + " was: " + str(pricedata[date]))
